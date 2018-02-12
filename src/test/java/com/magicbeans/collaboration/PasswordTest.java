f8c71f877540913d8c1b5e6201c2738ccc93024a
kalen <kalen>
2018-02-12 09:10:52 +0800

    创建项目

---
+++ src/test/java/com/magicbeans/collaboration/PasswordTest.java
@@ -0,0 +1,14 @@
+package com.magicbeans.collaboration;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordTest {
+
+    @Test
+    public void test(){
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("111111"));
+    }
+}
